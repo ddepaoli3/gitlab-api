75141edeb5685c5af2537b19543e8213e8132bad
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2020-01-21 08:52:05 +0100

    Add function to clone all group repo

--- gitlab-api-analisi.sh
+++ gitlab-api-analisi.sh
@@ -35,10 +35,35 @@ list_groups|while read line
 do
 folder=`echo $line|sed s+https://gitlab.com/groups/++ | awk {'print $1'}`
 group_id=`echo $line | awk {'print $2'}`
-echo mkdir -p $relative_path/$folder
+mkdir -p $relative_path/$folder
+done
+}
+
+clone_all_group_repos()
+{
+group_id=$1
+path=${2:-'.'}
+list_project $group_id | while read line
+do
+project_path=`echo $line | sed s+https://gitlab.com/++`
+echo git clone $line $path/$project_path
+done
+}
+
+clone_recursively_all_repos()
+{
+relative_path=${1:-'.'}
+list_groups|while read line
+do
+repo=`echo $line | awk {'print $1'}`
+folder=`echo $line|sed s+https://gitlab.com/groups/++ | awk {'print $1'}`
+group_id=`echo $line | awk {'print $2'}`
+echo git clone $repo $relative_path/$folder
 done
 }
 
 generate_folder_tree_from_groups ~/workspace-gitlab-clone
+#clone_recursively_all_repos ~/workspace-gitlab-clone
+clone_all_group_repos 5400554 ~/workspace-gitlab-clone
 #list_subgroups 1179730
 #.[] |  "\(.web_url) \(.id)"
