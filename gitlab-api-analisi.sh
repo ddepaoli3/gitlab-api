999b89d69759fd4e82c0306119cad3f5a026a26b
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2020-01-21 08:24:48 +0100

    Add nuew functions

--- gitlab-api-analisi.sh
+++ gitlab-api-analisi.sh
@@ -12,14 +12,42 @@ fi
 list_subgroups()
 {
 group=$1
-echo "Lista gruppi sotto xp-coast"
+echo "Groups under group $group"
 curl "https://gitlab.com/api/v4/groups/$group/subgroups?private_token=$TOKEN&per_page=999"|json_pp|jq '.[].web_url'
 }
 
 list_groups()
 {
-echo "Lista gruppi "
-curl "https://gitlab.com/api/v4/groups?private_token=$TOKEN&per_page=999"|json_pp|jq '.[].web_url'
+echo "Group list"
+curl "https://gitlab.com/api/v4/groups?private_token=$TOKEN&per_page=999"|json_pp| jq -r '.[] | "\(.web_url) \(.id)"'
 }
 
-list_subgroups 1179730
+list_project()
+{
+group_id=$1
+curl "https://gitlab.com/api/v4/groups/$group_id/projects?private_token=$TOKEN&per_page=999"|json_pp|jq '.[].web_url'
+}
+
+clone_recursive_all_repo()
+{
+group_folder=$1
+group_id=$2
+relative_path=${3:-'.'}
+echo "mkdir -p $relative_path/$group_folder"
+#list_project $group_id
+}
+
+generate_folder_tree_from_groups()
+{
+relative_path=${1:-'.'}
+list_groups|while read line
+do
+folder=`echo $line|sed s+https://gitlab.com/groups/++`
+clone_recursive_all_repo $folder $folder $relative_path
+done
+}
+
+generate_folder_tree_from_groups
+#list_subgroups 1179730
+#.[] |  "\(.web_url) \(.id)"
+#clone_recursive_all_repo xp-coast/il-sole-24-ore 6031671
